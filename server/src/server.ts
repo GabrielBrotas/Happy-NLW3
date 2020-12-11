f7c9f3d8d6e560ec06135b7d81c6a557e78bff0e
GabrielBrotas <gabrielbrotas22@gmail.com>
2020-12-11 20:21:54 -0300

    cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,13 +3,14 @@ import bodyParser from 'body-parser'
 import routes from './routes'
 import path from 'path'
 import errorHandler from './errors/handler'
+import cors from 'cors'
 
 import './database/connection'
-
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors())
 app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(errorHandler)
