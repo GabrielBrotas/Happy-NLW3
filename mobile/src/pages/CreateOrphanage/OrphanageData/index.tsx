eec1fd463e378b5e5da75e85f2d43ee7e883923e
GabrielBrotas <gabrielbrotas22@gmail.com>
2020-12-14 20:39:13 -0300

    create orphanate page finished

--- mobile/src/pages/CreateOrphanage/OrphanageData/index.tsx
+++ mobile/src/pages/CreateOrphanage/OrphanageData/index.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
+import { RectButton, ScrollView, Switch, TouchableOpacity } from 'react-native-gesture-handler';
+import {Feather} from '@expo/vector-icons'
+
+import styles from './styles'
 
 function OrphanageData() {
 
     return (
-        <View>
+        <ScrollView style={styles.container} contentContainerStyle={{padding: 24}}>
+            <Text style={styles.title}>Dados</Text>
+
+            <View style={styles.separator} />
+
+            <Text style={styles.label}>Nome</Text>
+            <TextInput style={styles.input} />
             
-        </View>
+            <Text style={styles.label}>Sobre</Text>
+            <TextInput style={[styles.input, {height: 100}]} multiline />
+
+            <Text style={styles.label}>Numero do Whatsapp</Text>
+            <TextInput style={styles.input} />
+    
+            <Text style={styles.label}>Fotos</Text>
+            <TouchableOpacity style={styles.pictureButton}>
+                <Feather name="plus" size={20} color="#96d2f0" />
+            </TouchableOpacity>
+
+            <Text style={styles.title}>Visitação</Text>
+            
+            <View style={styles.separator} />
+
+            <Text style={styles.label}>Instruções</Text>
+            <TextInput style={[styles.input, {height: 100}]} multiline/>
+    
+            <Text style={styles.label}>Horário das Visitas</Text>
+            <TextInput style={styles.input} />
+
+            <View style={styles.openOnWeekendsContent}>
+                <Text style={styles.label}>Atende final de semana?</Text>
+                <Switch 
+                    thumbColor="#fff"
+                    trackColor={{false: "#ccc", true: "#39cc83 "}}
+                />
+            </View>
+
+            <RectButton style={styles.nextButton} >
+                <Text style={styles.nextButtonText}>Próximo</Text>
+            </RectButton>
+
+        </ScrollView>
     )
 }
 
---
+++ mobile/src/pages/CreateOrphanage/OrphanageData/styles.ts
@@ -0,0 +1,68 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        color: "#5c8599",
+        fontSize: 24,
+        marginBottom: 25,
+        marginTop: 25
+    },
+    separator: {
+        height: 0.8,
+        width: '100%',
+        backgroundColor: "#d3e2e6",
+        marginBottom: 20    
+    },
+    label: {
+        color: "#5c8599",
+        lineHeight: 24,
+        marginBottom: 8
+    },
+    input: {
+        height: 56,
+        backgroundColor: "#fff",
+
+        borderWidth: 1.4,
+        borderColor: "#d3e2e6",
+        borderRadius: 20,
+
+        paddingVertical: 18,
+        paddingHorizontal: 24,
+
+        marginBottom: 16
+    },
+    pictureButton: {
+        backgroundColor: "#fff",
+        height: 50,
+        
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: "#96d2f0",
+        borderStyle: 'dotted',
+
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    openOnWeekendsContent: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: 20
+    },
+    nextButton: {
+        backgroundColor: '#15c3d6',
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 56,
+
+    },
+    nextButtonText: {
+        fontSize: 16,
+        color: "#fff"
+    }
+})
+
+export default styles
