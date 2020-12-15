eb15bf02c6134eb1642076e7c9f7653aae78f607
GabrielBrotas <gabrielbrotas22@gmail.com>
2020-12-15 20:08:01 -0300

    button restrict access landing

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -9,18 +9,24 @@ function Landing() {
     return (
         <div id="page-landing">
             <div className="content-wrapper">
-                <img src={logoImg} alt="happy" />
+                <header>
+                    <img src={logoImg} alt="happy" />
+                    <div className="location">
+                        <strong>Bahia</strong>
+                        <span>Camaçari</span>
+                    </div>
+                </header>
+
 
                 <main>
                     <h1>Leve felicidade para o mundo</h1>
                     <p>Visite orfanatos e mude o dia de muitas crianças</p>
                 </main>
 
-                <div className="location">
-                    <strong>Bahia</strong>
-                    <span>Camaçari</span>
-                </div>
-
+                <button className="button-restricted-access">
+                    Acesso Restrito
+                </button>
+                
                 <Link to="/app" className="enter-app">
                     <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
                 </Link>
