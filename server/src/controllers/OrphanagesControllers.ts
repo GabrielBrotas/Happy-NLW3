eec3fdd74e5e5d7afea959642da4339ee70701d7
GabrielBrotas <gabrielbrotas22@gmail.com>
2020-12-22 20:41:36 -0300

    update orphanage

--- server/src/controllers/OrphanagesControllers.ts
+++ server/src/controllers/OrphanagesControllers.ts
@@ -5,7 +5,47 @@ import {promisify} from 'util'
 import * as Yup from 'yup' // o yup é uma biblioteca de validação de dados, como ele não tem um export default vamos fazer uma importação de tudo usando o *
 import {getRepository} from 'typeorm' // getRepository = vai determinar todas as operações que formos fazer no DB, criar, deletar, listar, etc. o Repositorio vai deter todas essas regras
 import Orphanage from '../models/Orphanage'
+import Image from '../models/Image'
+
 import OrphanageView from '../views/orphanages_view'
+
+interface orphanageProps {
+  name: string, 
+  latitude: number, 
+  longitude: number, 
+  about: string, 
+  instructions: string, 
+  opening_hours: string, 
+  open_on_weekends: boolean; //converter para boolean 
+  images?: Array<{}>,
+  accepted: boolean
+
+}
+
+async function validateOrphanageData(data: orphanageProps) { 
+  // criando um schema/interface que nossos orfanatos deve ter
+  const schema = Yup.object().shape({
+    name: Yup.string().required(),
+    latitude: Yup.number().required(),
+    longitude: Yup.number().required(),
+    about: Yup.string().required().max(300),
+    instructions: Yup.string().required(),
+    opening_hours: Yup.string().required(),
+    open_on_weekends: Yup.boolean().required(),
+    images: Yup.array(
+      Yup.object().shape({
+        path: Yup.string().required(),
+    })
+    ),
+    accepted: Yup.boolean().required()
+  })
+
+  // validação
+  await schema.validate(data, {
+    abortEarly: false // se ele encontrar um campo que não estiver valido ele imediatamente vai abortar, vamos deixar falso pois queremos a mensagem de todos os campos que não estiverem validos, retornar todos os erros ao mesmo tempo.
+  })
+
+}
 
 export default {
 
@@ -61,27 +101,7 @@ export default {
       accepted: false
     }
 
-    // criando um schema/interface que nossos orfanatos deve ter
-    const schema = Yup.object().shape({
-      name: Yup.string().required(),
-      latitude: Yup.number().required(),
-      longitude: Yup.number().required(),
-      about: Yup.string().required().max(300),
-      instructions: Yup.string().required(),
-      opening_hours: Yup.string().required(),
-      open_on_weekends: Yup.boolean().required(),
-      images: Yup.array(
-        Yup.object().shape({
-          path: Yup.string().required(),
-      })
-      ),
-      accepted: Yup.boolean().required()
-    })
-
-    // validação
-    await schema.validate(data, {
-      abortEarly: false // se ele encontrar um campo que não estiver valido ele imediatamente vai abortar, vamos deixar falso pois queremos a mensagem de todos os campos que não estiverem validos, retornar todos os erros ao mesmo tempo.
-    })
+    await validateOrphanageData(data)
 
     // .create apenas vai deixar pre-criado o repositorio, não vai salvar
     const orphanage = orphanagesRepository.create(data)
@@ -92,7 +112,48 @@ export default {
   },
 
   async update(request: Request, response: Response) {
+    const {name, latitude, longitude, about, instructions, opening_hours, open_on_weekends} = request.body
+    const {id} = request.params
 
+    const orphanagesRepository = getRepository(Orphanage)
+    const imagesRepository = getRepository(Image)
+
+    const data = {
+      name, 
+      latitude, 
+      longitude, 
+      about, 
+      instructions, 
+      opening_hours, 
+      open_on_weekends: open_on_weekends === "true", //converter para boolean 
+      accepted: true 
+    }
+
+    await validateOrphanageData(data)
+
+    // .create apenas vai deixar pre-criado o repositorio, não vai salvar
+    const orphanage = await orphanagesRepository.findOne(id)
+   
+    imagesRepository.find({orphanage}).then( async orphanageImages => {
+      orphanageImages.map( image => {
+        imagesRepository.delete(image.id)
+        return promisify(fs.unlink)(path.resolve(__dirname, '..', '..', 'uploads', image.path))
+      })
+      
+      const requestImages = request.files as Express.Multer.File[];
+      
+      const images = requestImages.map( image => {
+        return {
+          path: image.filename,
+          orphanage
+        }
+      })
+
+      const image = imagesRepository.create(images)
+      await imagesRepository.save(image)
+    })
+    await orphanagesRepository.update(id, data)
+    return response.status(200).send("orphanage updated successfully")
   },
   
   async delete(request: Request, response: Response) {
