8fc07e6f782f7fa5a611c6aa90626cef312897e0
GabrielBrotas <gabrielbrotas22@gmail.com>
2020-12-22 20:42:22 -0300

    style

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,15 +7,15 @@ import 'dotenv/config'
 import OrphangesController from './controllers/OrphanagesControllers'
 import UsersController from './controllers/UsersControllers'
 
-const routes = Router()
-const upload = multer(uploadConfig)
+const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get('/orphanages/:accepted', OrphangesController.index)
 routes.get('/orphanage/:id', OrphangesController.show)
 routes.delete('/orphanage/:id', OrphangesController.delete)
 routes.post('/orphanages/accept-response/:id', OrphangesController.acceptOrphanageResponse)
 // upload.array(<nome do campo que vai receber as imagens>) = receber varias imagens ao mesmo tempo
-routes.post('/orphanages', upload.array('images'), OrphangesController.create);
+routes.post('/orphanages', upload.array('images'), OrphangesController.create)
 routes.put('/orphanage/:id', upload.array('images'), OrphangesController.update)
 
 routes.get('/user/:id', UsersController.getUserData)
